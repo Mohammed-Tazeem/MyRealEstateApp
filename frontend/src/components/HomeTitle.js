da89ad45da506207fb3d1cd52ef3c6a1fc6aa4c7
Mohammed <mtkhanalig@gmail.com>
2023-06-22 23:44:13 +0530

    fourth commit

---
+++ frontend/src/components/HomeTitle.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, Typography } from '@mui/material';
+import city from '../resources/city.jpg';
+import { useNavigate } from 'react-router-dom'
+
+function HomeTitle() {
+
+  const navigate = useNavigate()
+  return (
+    <div style={{ position: 'relative' }}>
+
+      <img alt='background' src={city} style={{ height: '92vh', width: '100%' }} />
+      <div style={{ zIndex: '100', position: 'absolute', top: '100px', left: '20px', textAlign: 'center' }}>
+        <Typography variant='h1'
+          style={{ color: 'white', fontWeight: 'bolder', textAlign: 'center' }}>
+          Find Your Next Property on LBREP
+        </Typography>
+
+        <Button variant='contained'
+          style={{
+            fontSize: '2.5rem',
+            borderRadius: '15px',
+            backgroundColor: 'green',
+            marginTop: '2rem',
+            boxShadow: '3px 3px 3px white',
+          }}
+          onClick={() => navigate('/listings')}
+        >
+          SEE All The Properties
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default HomeTitle
